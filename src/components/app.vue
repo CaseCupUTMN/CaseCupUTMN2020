a8799d1430e3396d23efe2cd40d4c1e51466c3f4
Maks <61799914+maaxnikolaevich@users.noreply.github.com>
2020-03-07 21:31:46 +0500

    Update app.vue

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,23 +1,11 @@
 <template>
 <f7-app :params="f7params" >
-
-  <!-- Left panel with cover effect-->
-  <f7-panel left cover theme-dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Left Panel"></f7-navbar>
-        <f7-block>Left panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
-
-
-  <!-- Right panel with reveal effect-->
+   <!-- Right panel with reveal effect-->
   <f7-panel right reveal theme-dark>
     <f7-view>
       <f7-page>
-        <f7-navbar title="Right Panel"></f7-navbar>
-        <f7-block>Right panel content goes here</f7-block>
+        <f7-navbar title="EncouterMe"></f7-navbar>
+        <f7-block></f7-block>
       </f7-page>
     </f7-view>
   </f7-panel>
@@ -27,75 +15,57 @@
   <f7-view main class="safe-areas" url="/"></f7-view>
 
 
-  <!-- Popup -->
-  <f7-popup id="my-popup">
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Popup">
-          <f7-nav-right>
-            <f7-link popup-close>Close</f7-link>
-          </f7-nav-right>
-        </f7-navbar>
-        <f7-block>
-          <p>Popup content goes here.</p>
-        </f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-popup>
-<!--:opened="loginScreenOpened" @loginscreen:opened="loginScreenOpened = true"-->
- <f7-login-screen id="login-screen" theme-dark >
+  
+<f7-login-screen id="login-screen" theme-dark>
     <f7-view>
       <f7-page login-screen>
-        <f7-login-screen-title>Login</f7-login-screen-title>
+        <f7-navbar back-link="" ></f7-navbar>
+        <f7-login-screen-title>Вход</f7-login-screen-title>
         <f7-list form>
           <f7-list-input
             type="text"
-            name="username"
-            placeholder="Your username"
+            placeholder="Имя пользователя или почта"
             :value="username"
             @input="username = $event.target.value"
           ></f7-list-input>
           <f7-list-input
             type="password"
-            name="password"
-            placeholder="Your password"
+         placeholder="Пароль"
             :value="password"
             @input="password = $event.target.value"
           ></f7-list-input>
         </f7-list>
         <f7-list>
-          <f7-list-button title="Sign In" @click="alertLoginData"></f7-list-button>
+<f7-button fill @click="alertLoginData">Войти</f7-button>
           <f7-block-footer>
-            Some text about login information.<br>Click "Sign In" to close Login Screen
+          Еще не создали аккаунт?<br><f7-list-button >Зарегистрироваться</f7-list-button>
           </f7-block-footer>
         </f7-list>
       </f7-page>
     </f7-view>
   </f7-login-screen>
-  <f7-login-screen id="register-screen" theme-dark>
+  <f7-login-screen id="register-screen" theme-dark >
     <f7-view>
-      <f7-page login-screen>
-        <f7-login-screen-title>Register</f7-login-screen-title>
+      <f7-page login-screen >
+        <f7-navbar back-link="Back" ></f7-navbar>
+        <f7-login-screen-title>Регистрация</f7-login-screen-title>
         <f7-list form>
           <f7-list-input
             type="text"
-            name="username"
-            placeholder="Your username"
+          placeholder="Имя пользователя или почта"
             
             @input="username = $event.target.value"
           ></f7-list-input>
           <f7-list-input
             type="password"
-            name="password"
-            placeholder="Your password"
-            
+           placeholder="Пароль"
             @input="password = $event.target.value"
           ></f7-list-input>
         </f7-list>
         <f7-list>
-          <f7-list-button title="Sign Up" @click="alertRegisterData"></f7-list-button>
+          <f7-button fill @click="alertRegisterData">Зарегистрироваться</f7-button>
           <f7-block-footer>
-            Some text about login information.<br>Click "Sign In" to close Login Screen
+            Уже есть аккаунт?<f7-list-button title="Войти"></f7-list-button>
           </f7-block-footer>
         </f7-list>
       </f7-page>
