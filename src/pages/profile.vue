2aa5ebebaa236a40ccc9786ef109ec9e4706b44f
Maks <61799914+maaxnikolaevich@users.noreply.github.com>
2020-03-07 21:32:58 +0500

    Update and rename form.vue to profile.vue

---
+++ src/pages/profile.vue
@@ -0,0 +1,54 @@
+<template>
+  <f7-page name="Profile" theme-dark >
+    <f7-navbar title="EncouterMe" back-link="Back"></f7-navbar>
+    <f7-block-title>Мой профиль</f7-block-title>
+    <f7-list no-hairlines-md>
+      <f7-list-input
+        type="text"
+        placeholder="Ваше имя"
+        :value="username"
+        @input="username = $event.target.value"
+      ></f7-list-input>
+
+      <f7-list-input
+        label="E-mail"
+        type="email"
+        placeholder="E-mail"
+      ></f7-list-input>
+      <f7-list-input
+
+        type="password"
+        placeholder="Ваш пароль"
+         :value="password"
+         @input="password = $event.target.value"
+      ></f7-list-input>
+
+      <f7-list-input
+        type="tel"
+        placeholder="Телефон"
+      ></f7-list-input>
+
+      <f7-list-input
+        label="Пол"
+        type="select"
+        >
+        <option>Не выбрано</option>
+        <option>Мужской</option>
+        <option>Женский</option>
+      </f7-list-input>
+
+      <f7-list-input
+        type="date"
+        placeholder="Дата рождения"
+      ></f7-list-input>
+
+
+      <f7-list-input
+        type="textarea"
+        placeholder="О себе"
+      ></f7-list-input>
+    </f7-list>
+   
+    
+    </f7-page>
+</template>
