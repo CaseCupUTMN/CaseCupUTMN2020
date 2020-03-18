055eaedff30d8fd127a20a3f2566949247e4e5a5
Maks <lebedkom03@gmail.com>
2020-03-18 05:05:00 +0500

    refresh+экран игры со стилем less

---
+++ src/pages/game.vue
@@ -0,0 +1,159 @@
+<template>
+  <f7-page name="game" tabs :page-content="false" animated>
+    <f7-toolbar tabbar bottom labels>
+      <f7-link tab-link-active tab-link="#info" text="Инфо" icon-f7="info_circle"></f7-link>
+      <f7-link tab-link="#points" text="Точки" icon-f7="placemark"></f7-link>
+      <f7-link tab-link="#rating" text="Рейтинг" icon-f7="stopwatch"></f7-link>
+    </f7-toolbar>
+    <f7-tabs animated>
+      <f7-page-content tab tab-active id="info">
+      <f7-block-title medium>Игра</f7-block-title>
+ <f7-block-title class="rectangle">Название:</f7-block-title>
+  <f7-block >
+    <p> </p>
+  </f7-block>
+<f7-block-title class="rectangle" >Дата и время проведения:</f7-block-title>
+  <f7-block>
+    <p> </p>
+  </f7-block>
+  <f7-block-title class="rectangle">Дата окончания:</f7-block-title>
+  <f7-block>
+    <p> </p>
+  </f7-block>
+  <f7-block-title class="rectangle">Участники:</f7-block-title>
+  <f7-block>
+    <p> </p>
+  </f7-block>
+  <f7-block-title class="rectangle">Описание:</f7-block-title>
+  <f7-block>
+    <p> </p>
+  </f7-block>
+  
+      </f7-page-content>
+      <f7-page-content tab id="points" @tab:show="onTabPointsShow" @tab:hide="onTabPointsHide">
+        <div id="map" style="width: 100%; height: 100%"></div>
+      </f7-page-content>
+
+      <f7-page-content tab id="rating">
+        <template v-if="!rating.length">
+          <f7-block>
+            <f7-block-footer class="text-align-center">Нет участников</f7-block-footer>
+          </f7-block>
+        </template>
+        <template v-else>
+          <f7-list media-list class="no-margin-vertical">
+            <f7-list-item v-for="(item, i) in rating" :key="item.id"
+              :title="item.user"
+              :after="item.time"
+              :text="`${item.points}/${points} точек`"
+            >
+              <div class="place" slot="media">{{i+1}}</div>
+            </f7-list-item>
+          </f7-list>
+        </template>
+      </f7-page-content>
+    </f7-tabs>
+  </f7-page>
+</template>
+
+<style lang="less" scoped>
+  .place {
+    font-size: 1.3em;
+    line-height: 36px;
+    text-align: center;
+    border-radius: 50%;
+    background: #dedede;
+    width: 36px;
+    height: 36px;
+  }
+  
+  
+  .rectangle {
+    font-size: 1.3em;
+    line-height: 36px;
+    text-indent: 10px;
+    background: #dedede;
+    height: 36px;
+}
+</style>
+
+<script>
+let map = null;
+let coordinates=[[57.153033, 65.534328],[57.154033, 65.534128]];
+let codes=[12,11];
+let isclick=[true,true];
+export default {
+  data() {
+    return {
+      game: {
+      title: ("Игра 1")
+      },
+      points: 14,
+      rating: []
+    }
+  },
+
+  methods: {
+    onTabPointsShow() {
+      ymaps.ready(() => {
+        map = new ymaps.Map("map", {
+          center: [57.153033, 65.534328],
+          zoom: 12,
+          controls: []
+        });
+
+
+      });
+    },
+
+    onTabPointsHide() {
+      if (map) {
+        map.destroy();
+      }
+    }
+  },
+   mounted() {
+     this.$f7ready((f7) => {
+        ymaps.ready(() => {
+          map = new ymaps.Map("map", {
+            center: [57.153033, 65.534328],
+            zoom: 12,
+            controls: ['zoomControl']
+          });
+          var myCollection = new ymaps.GeoObjectCollection({}, {
+            preset: 'islands#greenIcon', //все метки зеленые
+            //draggable: true // и их можно перемещать
+          });
+          var myGeoObject;
+          for(let i=0;i<coordinates.length;i++){ 
+            myGeoObject = new ymaps.GeoObject({
+              geometry: {
+                  type: "Point", // тип геометрии - точка
+                  coordinates: [coordinates[i][0], coordinates[i][1]] // координаты точки
+                }
+            });
+            myGeoObject.events.add('click', function (e) {
+              var coords = e.get('coords');
+              if(isclick[i]){
+              let code=prompt('Введите код ');
+              if(code){
+                if(code==codes[i]){
+                  e.get('target').options.set('preset', 'islands#greenIcon');
+                  //map.events.remove("click", onMapClick);
+                  isclick[i]=false;
+                }else{
+                  e.get('target').options.set('preset', 'islands#redIcon');
+                }
+              }
+              }
+            });
+            map.geoObjects.add(myGeoObject);
+          }
+         
+
+        });
+     });
+
+   }
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,6 +9,7 @@
       <f7-nav-title-large sliding>EncounterMe</f7-nav-title-large>
     </f7-navbar>
 
+<<<<<<< Updated upstream
     <!-- Page content-->
     <f7-block strong>
       <p>Игры, в которых вы зарегистрированны.</p>
@@ -17,3 +18,142 @@
     
   </f7-page>
 </template>
+=======
+    <f7-tabs animated swipeable>
+      <f7-page-content tab tab-active id="all-games" infinite @infinite:refresh="refreshAll"> 
+        <f7-fab position="right-bottom" color="orange" href="/new-game/" >
+          <f7-icon f7="plus"></f7-icon>
+        </f7-fab>
+        
+        <template v-if="!games.all.length">
+          <f7-block>
+            <f7-block-footer class="text-align-center">Не запланировано ни одной игры</f7-block-footer>
+          </f7-block>
+        </template>
+        <template v-else>
+          <f7-list media-list class="no-margin-vertical">
+            <f7-list-item v-for="item in games.all" :key="item.id"
+              :title="item.title"
+              :link="`/game/${item.id}`"
+              :after="`${item.players.current}/${item.players.max} чел.`"
+              :subtitle="formatDate(item.startDate)"
+              :text="item.description"
+              chevron-center
+            ></f7-list-item> 
+           
+          </f7-list>
+          
+        </template>
+       
+      </f7-page-content>
+
+      <f7-page-content tab id="current-games" ptr @ptr:refresh="refreshCurrent">
+        <template v-if="!games.current.length">
+        <f7-block >
+          <f7-block-footer class="text-align-center">Вы не зарегистрированы ни в одной игре</f7-block-footer>
+        </f7-block>
+        </template>
+        <template v-else>
+        
+           <f7-list media-list class="no-margin-vertical">
+            <f7-list-item v-for="item in games.current.all" :key="item.id"
+              :title="item.title"
+              :link="`/game/${item.id}`"
+              :after="`${item.players.current}/${item.players.max} чел.`"
+              :subtitle="formatDate(item.startDate)"
+              :text="item.description"
+              chevron-center
+            ></f7-list-item>
+          </f7-list>
+       
+        </template>
+      </f7-page-content>
+      
+    </f7-tabs>
+  </f7-page>
+</template>
+<style>
+.fab{
+      position: fixed;
+}
+</style>
+<script>
+import moment from 'moment';
+export default {
+  data() {
+    return {
+      loadInProgress: false,
+      games: {
+        all: [
+          {title:"Знакомство с Тюменью",
+                      id:1,
+                      players:{
+                        current:1,
+                        max:50
+                      },
+                      startDate:"03.15.2020",//(месяц,день,год)
+                      description:"ВЫ пройдете все достопримечательности города Тюмени"
+                    },
+        ],
+        current: [
+          
+        ]
+      }
+    }
+  },
+
+  methods: {
+    tabAllShow() {},
+    refreshPush(done){
+      alert("YES");
+      setTimeout(() => {
+        
+        for(let i=20;i<40;i++){
+          this.games.all.push({title:"newgame2",
+                      id:i+1*2,
+                      players:{
+                        current:i,
+                        max:50
+                      },
+                      startDate:"02.02.2020",
+                      description:"Просто текст"
+                    });
+        }
+        
+        done();
+      }, 1000);
+    },
+    refreshAll(done) {
+      setTimeout(() => {
+        for(let i=0;i<19;i++){
+          this.games.all.push({title:"newgame1",
+                      id:i+1*2,
+                      players:{
+                        current:i,
+                        max:50
+                      },
+                      startDate:"02.02.2020",
+                      description:"Просто текст"
+                    });
+        }
+        
+        done();
+      }, 1000);
+    },
+
+
+
+    formatDate(value) {
+      
+      //return value ? moment(value).format('DD.MM.YY в HH:mm') : 'дата неизвестна';
+      return value ? moment(value).format('DD.MM.YY') : 'дата неизвестна';
+    }
+  },
+  mounted() {
+    this.$f7ready((f7) => {
+       
+    });
+  }
+}
+</script>
+>>>>>>> Stashed changes
